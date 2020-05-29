ec9f14d377b102d15c9b380876be6f454fd5e3dc
Kirk Charles Niverba <kirkniverba23@gmail.com>
2020-05-29 15:45:30 +0800

    Sign In Page Design Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -26,145 +28,9 @@ class SignUp extends StatefulWidget {
   _SignUpState createState() => _SignUpState();
 }
 
-class _SignUpState extends State<SignUp> with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-
-  final _formKey = GlobalKey<FormState>();
-  bool isDismissing = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 3500),
-      vsync: this,
-    );
-    _controller.forward();
-    isDismissing = false;
-  }
-
+class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            child: FlareActor(
-              "assets/animations/springboard_login.flr",
-              animation: isDismissing ? "reverse" : "forward",
-              fit: BoxFit.cover,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  'Springboard',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 32),
-                ),
-              ],
-            ),
-          ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(
-                        height: MediaQuery.of(context).size.height * 0.425),
-                    Form(
-                      key: _formKey,
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(
-                            decoration: InputDecoration(hintText: 'Email'),
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(hintText: 'Password'),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'Sign In',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 34),
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            gradient: LinearGradient(
-                              begin: Alignment.bottomLeft,
-                              end: Alignment.topRight,
-                              colors: [Color(0xff3D404A), Color(0xff545C67)],
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(28.0),
-                            child: Icon(
-                              Icons.arrow_forward,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 45,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        FlatButton(
-                          onPressed: () => print('Sign Up'),
-                          child: Text(
-                            'Sign Up',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 22,
-                              decoration: TextDecoration.underline,
-                            ),
-                          ),
-                        ),
-                        FlatButton(
-                          onPressed: () => print('Forgot Password'),
-                          child: Text(
-                            'Forgot Password',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 22,
-                              decoration: TextDecoration.underline,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return Container();
   }
 }
---
+++ lib/screens/sign_in.dart
@@ -0,0 +1,220 @@
+import 'dart:async';
+
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  final _formKey = GlobalKey<FormState>();
+  bool isDismissing = false;
+
+  // Staggered Animation
+  Animation<double> pItem1;
+  Animation<double> pItem2;
+  Animation<double> pItem3;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Start Login Animation
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 3500),
+      vsync: this,
+    );
+    isDismissing = false;
+
+    // Start Staggered Animation
+    Timer(Duration(milliseconds: 1600), () {
+      print('Starting staggered animation');
+      _controller.forward();
+    });
+
+    pItem1 = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Interval(
+          0.0, // 1
+          0.5,
+          curve: Curves.ease,
+        ),
+      ),
+    );
+
+    pItem2 = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Interval(
+          0.25, // 1
+          0.75,
+          curve: Curves.ease,
+        ),
+      ),
+    );
+
+    pItem3 = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Interval(
+          0.5, // 1
+          1,
+          curve: Curves.ease,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: FlareActor(
+              "assets/animations/springboard_login.flr",
+              animation: isDismissing ? "reverse" : "forward",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Springboard',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 32),
+                ),
+              ],
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: SingleChildScrollView(
+                child: AnimatedBuilder(
+                  animation: _controller,
+                  builder: (context, child) => Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.425),
+                      Opacity(
+                        opacity: pItem1.value,
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                decoration: InputDecoration(hintText: 'Email'),
+                              ),
+                              TextFormField(
+                                decoration:
+                                    InputDecoration(hintText: 'Password'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Opacity(
+                        opacity: pItem2.value,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Sign In',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 34),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                gradient: LinearGradient(
+                                  begin: Alignment.bottomLeft,
+                                  end: Alignment.topRight,
+                                  colors: [
+                                    Color(0xff3D404A),
+                                    Color(0xff545C67)
+                                  ],
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(28.0),
+                                child: Icon(
+                                  Icons.arrow_forward,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 45,
+                      ),
+                      Opacity(
+                        opacity: pItem3.value,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            FlatButton(
+                              onPressed: () {
+                                setState(() {
+                                  isDismissing = true;
+                                  _controller.reverse().then((_) {
+                                    print('Navigate on Sign Up');
+                                  });
+                                });
+                              },
+                              child: Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                            FlatButton(
+                              onPressed: () => print('Forgot Password'),
+                              child: Text(
+                                'Forgot Password',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 22,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
